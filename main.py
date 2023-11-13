f067f4babd77872637e5195d70bda2f50311e843
bgallll <baxterg@udel.edu>
2023-11-13 10:42:40 -0500

    Added apples that will fall from the sky and be consumed by the cow

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from random import randint
 
 # Set speed of the copter
 COW_SPEED = 15
+APPLE_FALLING_SPEED = 10
 
 
 @dataclass
@@ -11,9 +12,11 @@ class World:
     sky: DesignerObject
     cow: DesignerObject
     grass: DesignerObject
+    apples: list[DesignerObject]
     cow_speed: int
     platforms: list[DesignerObject]
     timer: int
+
 
 def create_sky() -> DesignerObject:
     """ Create the sky """
@@ -41,7 +44,7 @@ def create_grass() -> DesignerObject:
 
 def create_world() -> World:
     """ Create the world """
-    return World(create_sky(), create_cow(), create_grass(), COW_SPEED, 0, 0)
+    return World(create_sky(), create_cow(), create_grass(), [], COW_SPEED, 0, [])
 
 
 def head_left(world: World):
@@ -74,6 +77,46 @@ def flip_cow(world: World, key: str):
         head_right(world)
 
 
+def create_apple() -> DesignerObject:
+    """ Create a small apple randomly on the top of the screen """
+    apple = emoji('🍎')
+    apple.scale_x = 3
+    apple.scale_y = 4
+    apple.anchor = 'midtop'
+    apple.x = randint(0, get_width())
+    apple.y = 0
+    return apple
+
+
+def create_new_apple(world: World):
+    """ Create a new apple randomly"""
+    if len(world.apples) < 10 and randint(1, 100) == 50:
+        new_apple = create_apple()
+        world.apples.append(new_apple)
+
+
+def cow_eats_apples(world: World):
+    eaten_apples = []
+    destroyed_drops = []
+    for apple in world.apples:
+        if colliding(apple, world.cow):
+            world.cow.scale_x += 1
+            world.cow.scale_y += 1
+            eaten_apples.append(apple)
+            destroyed_drops.append(apple)
+    for apple in eaten_apples:
+        world.apples.remove(apple)
+
+
+def apples_falling(world: World):
+    """ Make the apples fall """
+    for falling_apple in world.apples:
+        falling_apple.y += APPLE_FALLING_SPEED
+
+
+when("updating", apples_falling)
+when("updating", create_new_apple)
+when("updating", cow_eats_apples)
 when("typing", flip_cow)
 when('starting', create_world)
 when("updating", bounce_cow)
